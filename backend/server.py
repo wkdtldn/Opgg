ac554cdfddda8f4d969e414daf5034d4fbd1e449
ICUSXD <you@example.com>
2023-12-10 16:03:39 +0900

    :memo:update

--- backend/server.py
+++ backend/server.py
@@ -8,17 +8,21 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-userData = list()
+Data = list()
 API_key = 'RGAPI-d6b46f86-ce5f-498b-a30a-f3660691f317';
 
+## UserInfo에 들어갈 Data
+userData = { "level" : "", "name" : "", "tag" : "", "tier" : "" , "rank" : "" }
+MatchInfo = list()
+
 class UserInfo:
-    def __init__(self, id, puuid, summonerid, name, tag):
-        self.id = id
-        self.puuid = puuid
-        self.summonerid = summonerid
+    def __init__(self, level, name, tag, tier, rank, match):
+        self.level = level
         self.name = name
         self.tag = tag
-
+        self.tier = tier
+        self.rank = rank
+        self.match = match
 
 
 @app.route('/',methods=['GET', 'POST'])
@@ -28,19 +32,30 @@ def main():
     res_puuid = requests.get("https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" + userName + "/" + userTag + "?api_key=" + API_key)
     if res_puuid.status_code == 200:
         res_id = requests.get("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + userName + "?api_key=" + API_key)
-        res_summonerid = requests.get("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + userName + "?api_key=" + API_key)
-        collect(res_puuid,"puuid")
-        collect(res_id,"id")
-        collect(res_summonerid,"summonerid")
+        user_info_collect(res_puuid)
+        user_info_collect(res_id)
+        res_tier = requests.get("https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + Data[1]["id"] + "?api_key=" + API_key)
+        res_matchID = requests.get("https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" + Data[0]["puuid"] +"/ids?type=normal&count=1&api_key=" + API_key)
+        user_info_collect(res_tier)
+        user_info_collect(res_matchID)        
+        res_matchInfo = requests.get("https://asia.api.riotgames.com/lol/match/v5/matches/" + Data[3][0] + "?api_key=" + API_key)
+        user_info_collect(res_matchInfo)
+        userData["level"] = Data[1]["summonerLevel"]
+        userData["name"] = Data[0]["gameName"]
+        userData["tag"] = Data[0]["tagLine"]
+        userData["tier"] = Data[2][0]["tier"]
+        userData["rank"] = Data[2][0]["rank"]
+        MatchInfo = Data[4]
+        print(MatchInfo)
         return jsonify(userData)
     else:
         return jsonify({'Result' : False})
 
-def collect(item,name):
+def user_info_collect(item):
     data = item.text
     data = json.loads(data)
-    userData.append({ name : data })
-    return jsonify({ name : data})
+    Data.append(data)
+    
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
---
+++ backend/test.py
@@ -0,0 +1,37 @@
+userData = [
+  {
+    "gameName": "jolf",
+    "puuid": "2B41b8UtwGaOFyBRb7SzCT7oCnkQcJRkOmzstF2vyFL1e3fPGCuZvRj2c-P4lL84YfnRZbh63aPTQQ",
+    "tagLine": "KR1"
+  },
+  {
+    "accountId": "4L7qBPNn4cOMKCcqdzaMz1DJrAfApMpvfGWhYArjQ3y3e15BOIi94vlG",
+    "id": "FqsQh2bZpipNBOxGQExsIE_H9pAPUC9AfMS-A6gs06wbE-PJVSgzHzL8Vw",
+    "name": "jolf",
+    "profileIconId": 6,
+    "puuid": "2B41b8UtwGaOFyBRb7SzCT7oCnkQcJRkOmzstF2vyFL1e3fPGCuZvRj2c-P4lL84YfnRZbh63aPTQQ",
+    "revisionDate": 1702129257988,
+    "summonerLevel": 66
+  },
+  [
+    {
+      "freshBlood": "false",
+      "hotStreak": "false",
+      "inactive": 'false',
+      "leagueId": "7231e812-efa1-45d7-8250-adc9081c71db",
+      "leaguePoints": 15,
+      "losses": 6,
+      "queueType": "RANKED_SOLO_5x5",
+      "rank": "II",
+      "summonerId": "FqsQh2bZpipNBOxGQExsIE_H9pAPUC9AfMS-A6gs06wbE-PJVSgzHzL8Vw",
+      "summonerName": "jolf",
+      "tier": "IRON",
+      "veteran": 'false',
+      "wins": 1
+    }
+  ],
+  [
+    "KR_6837262539"
+  ]
+]
+print(userData[3][0])
