4d3d673882f3b24d3b9a0d9b34e80d57b4a0b811
wkdtldn <jsw.mat18@gmail.com>
2024-01-07 16:13:09 +0900

    first commit

---
+++ backend/UserData.py
@@ -0,0 +1,307 @@
+userInfo = { "level" : "", "name" : "", "tag" : "", "tier" : "" , "rank" : "" }
+matchData = [
+    {
+        "team" : "",
+        "position" : "",
+        "name" : "",
+        "tag" : "",
+        "puuid" : "",
+        "championName" : "",
+        "championLevel" : "",
+        "kills" : "",
+        "deaths" : "",
+        "assists" : "",
+        "kda" : "",
+        "damageToChampions" : "",
+        "damageTaken" : "",
+        "cs" : "" ,
+        "visionWardsBoughtInGame" : "",
+        "wardsKilled" : "",
+        "wardsPlaced" : "",
+        "items" :
+            {
+                0 : "", 1 : "",
+                2 : "", 3 : "",
+                4 : "", 5 : "",
+                6 : ""
+            },
+        "win" : ""
+    },
+    {
+        "team" : "",
+        "position" : "",
+        "name" : "",
+        "tag" : "",
+        "puuid" : "",
+        "championName" : "",
+        "championLevel" : "",
+        "kills" : "",
+        "deaths" : "",
+        "assists" : "",
+        "kda" : "",
+        "damageToChampions" : "",
+        "damageTaken" : "",
+        "cs" : "" ,
+        "visionWardsBoughtInGame" : "",
+        "wardsKilled" : "",
+        "wardsPlaced" : "",
+        "items" :
+            {
+                0 : "", 1 : "",
+                2 : "", 3 : "",
+                4 : "", 5 : "",
+                6 : ""
+            },
+        "win" : ""
+    },
+    {
+        "team" : "",
+        "position" : "",
+        "name" : "",
+        "tag" : "",
+        "puuid" : "",
+        "championName" : "",
+        "championLevel" : "",
+        "kills" : "",
+        "deaths" : "",
+        "assists" : "",
+        "kda" : "",
+        "damageToChampions" : "",
+        "damageTaken" : "",
+        "cs" : "" ,
+        "visionWardsBoughtInGame" : "",
+        "wardsKilled" : "",
+        "wardsPlaced" : "",
+        "items" :
+            {
+                0 : "", 1 : "",
+                2 : "", 3 : "",
+                4 : "", 5 : "",
+                6 : ""
+            },
+        "win" : ""
+    },
+    {
+        "team" : "",
+        "position" : "",
+        "name" : "",
+        "tag" : "",
+        "puuid" : "",
+        "championName" : "",
+        "championLevel" : "",
+        "kills" : "",
+        "deaths" : "",
+        "assists" : "",
+        "kda" : "",
+        "damageToChampions" : "",
+        "damageTaken" : "",
+        "cs" : "" ,
+        "visionWardsBoughtInGame" : "",
+        "wardsKilled" : "",
+        "wardsPlaced" : "",
+        "items" :
+            {
+                0 : "", 1 : "",
+                2 : "", 3 : "",
+                4 : "", 5 : "",
+                6 : ""
+            },
+        "win" : ""
+    },
+    {
+        "team" : "",
+        "position" : "",
+        "name" : "",
+        "tag" : "",
+        "puuid" : "",
+        "championName" : "",
+        "championLevel" : "",
+        "kills" : "",
+        "deaths" : "",
+        "assists" : "",
+        "kda" : "",
+        "damageToChampions" : "",
+        "damageTaken" : "",
+        "cs" : "" ,
+        "visionWardsBoughtInGame" : "",
+        "wardsKilled" : "",
+        "wardsPlaced" : "",
+        "items" :
+            {
+                0 : "", 1 : "",
+                2 : "", 3 : "",
+                4 : "", 5 : "",
+                6 : ""
+            },
+        "win" : ""
+    },
+    {
+        "team" : "",
+        "position" : "",
+        "name" : "",
+        "tag" : "",
+        "puuid" : "",
+        "championName" : "",
+        "championLevel" : "",
+        "kills" : "",
+        "deaths" : "",
+        "assists" : "",
+        "kda" : "",
+        "damageToChampions" : "",
+        "damageTaken" : "",
+        "cs" : "" ,
+        "visionWardsBoughtInGame" : "",
+        "wardsKilled" : "",
+        "wardsPlaced" : "",
+        "items" :
+            {
+                0 : "", 1 : "",
+                2 : "", 3 : "",
+                4 : "", 5 : "",
+                6 : ""
+            },
+        "win" : ""
+    },
+    {
+        "team" : "",
+        "position" : "",
+        "name" : "",
+        "tag" : "",
+        "puuid" : "",
+        "championName" : "",
+        "championLevel" : "",
+        "kills" : "",
+        "deaths" : "",
+        "assists" : "",
+        "kda" : "",
+        "damageToChampions" : "",
+        "damageTaken" : "",
+        "cs" : "" ,
+        "visionWardsBoughtInGame" : "",
+        "wardsKilled" : "",
+        "wardsPlaced" : "",
+        "items" :
+            {
+                0 : "", 1 : "",
+                2 : "", 3 : "",
+                4 : "", 5 : "",
+                6 : ""
+            },
+        "win" : ""
+    },
+    {
+        "team" : "",
+        "position" : "",
+        "name" : "",
+        "tag" : "",
+        "puuid" : "",
+        "championName" : "",
+        "championLevel" : "",
+        "kills" : "",
+        "deaths" : "",
+        "assists" : "",
+        "kda" : "",
+        "damageToChampions" : "",
+        "damageTaken" : "",
+        "cs" : "" ,
+        "visionWardsBoughtInGame" : "",
+        "wardsKilled" : "",
+        "wardsPlaced" : "",
+        "items" :
+            {
+                0 : "", 1 : "",
+                2 : "", 3 : "",
+                4 : "", 5 : "",
+                6 : ""
+            },
+        "win" : ""
+    },
+    {
+        "team" : "",
+        "position" : "",
+        "name" : "",
+        "tag" : "",
+        "puuid" : "",
+        "championName" : "",
+        "championLevel" : "",
+        "kills" : "",
+        "deaths" : "",
+        "assists" : "",
+        "kda" : "",
+        "damageToChampions" : "",
+        "damageTaken" : "",
+        "cs" : "" ,
+        "visionWardsBoughtInGame" : "",
+        "wardsKilled" : "",
+        "wardsPlaced" : "",
+        "items" :
+            {
+                0 : "", 1 : "",
+                2 : "", 3 : "",
+                4 : "", 5 : "",
+                6 : ""
+            },
+        "win" : ""
+    },
+    {
+        "team" : "",
+        "position" : "",
+        "name" : "",
+        "tag" : "",
+        "puuid" : "",
+        "championName" : "",
+        "championLevel" : "",
+        "kills" : "",
+        "deaths" : "",
+        "assists" : "",
+        "kda" : "",
+        "damageToChampions" : "",
+        "damageTaken" : "",
+        "cs" : "" ,
+        "visionWardsBoughtInGame" : "",
+        "wardsKilled" : "",
+        "wardsPlaced" : "",
+        "items" :
+            {
+                0 : "", 1 : "",
+                2 : "", 3 : "",
+                4 : "", 5 : "",
+                6 : ""
+            },
+        "win" : ""
+    },
+    { "playtime" : "" }
+] 
+
+def user_data_set(data):
+    userInfo["level"] = data["summonerLevel"]
+    userInfo["name"] = data["gameName"]
+    userInfo["tag"] = data["tagLine"]
+    userInfo["tier"] = data["tier"]
+    userInfo["rank"] = data["rank"]
+    return userInfo
+    
+def match_data_set(data):
+    for i in range(10):
+        matchData[i]["team"] = data["matchInfo"]["info"]["participants"][i]["teamId"]
+        matchData[i]["name"] = data["matchInfo"]["info"]["participants"][i]["riotIdGameName"]
+        matchData[i]["tag"] = data["matchInfo"]["info"]["participants"][i]["riotIdTagline"]
+        matchData[i]["puuid"] = data["matchInfo"]["info"]["participants"][i]["puuid"]
+        matchData[i]["position"] = data["matchInfo"]["info"]["participants"][i]["teamPosition"]
+        matchData[i]["championName"] = data["matchInfo"]["info"]["participants"][i]["championName"]
+        matchData[i]["championLevel"] = data["matchInfo"]["info"]["participants"][i]["champLevel"]
+        matchData[i]["kills"] = data["matchInfo"]["info"]["participants"][i]["kills"]
+        matchData[i]["deaths"] = data["matchInfo"]["info"]["participants"][i]["deaths"]
+        matchData[i]["assists"] = data["matchInfo"]["info"]["participants"][i]["assists"]
+        matchData[i]["kda"] = data["matchInfo"]["info"]["participants"][i]["challenges"]["kda"]
+        matchData[i]["damageToChampions"] = data["matchInfo"]["info"]["participants"][i]["totalDamageDealtToChampions"]
+        matchData[i]["damageTaken"] = data["matchInfo"]["info"]["participants"][i]["totalDamageTaken"]
+        matchData[i]["cs"] = data["matchInfo"]["info"]["participants"][i]["totalMinionsKilled"]
+        matchData[i]["visionWardsBoughtInGame"] = data["matchInfo"]["info"]["participants"][i]["visionWardsBoughtInGame"]
+        matchData[i]["wardsKilled"] = data["matchInfo"]["info"]["participants"][i]["wardsKilled"]
+        matchData[i]["wardsPlaced"] = data["matchInfo"]["info"]["participants"][i]["wardsPlaced"]
+        matchData[i]["win"] = data["matchInfo"]["info"]["participants"][i]["win"]
+        for j in range(7):
+            matchData[i]["items"][j] = data["matchInfo"]["info"]["participants"][i]["item" + str(j)]
+        matchData[10]["playtime"] = data["matchInfo"]["info"]["participants"][i]["timePlayed"] // 0.6
+    return matchData
